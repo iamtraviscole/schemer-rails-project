1149bb9f87827a5cf91c29e920cc1c5c228681e7
Travis Cole <iamtraviscole@gmail.com>
2017-09-15 23:50:12 -0400

    added color-picker and css assets, created color scheme create form and controller methods

--- app/controllers/color_schemes_controller.rb
+++ app/controllers/color_schemes_controller.rb
@@ -10,9 +10,20 @@ class ColorSchemesController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @color_scheme = ColorScheme.new
+    5.times do
+      @color_scheme.colors.new
+    end
   end
 
   def create
+    @color_scheme = current_user.color_schemes.new(color_scheme_params)
+    if @color_scheme.save
+      redirect_to user_color_schemes_path
+    else
+      redirect_to new_user_color_scheme_path
+    end
   end
 
   def edit
@@ -22,6 +33,12 @@ class ColorSchemesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def color_scheme_params
+    params.require(:color_scheme).permit(:name, colors_attributes: [:hex_code])
   end
 
 end
--- app/models/color_scheme.rb
+++ app/models/color_scheme.rb
@@ -4,4 +4,11 @@ class ColorScheme < ApplicationRecord
   has_many :favorited_by, through: :favorite_color_schemes, source: :user
   has_many :color_scheme_colors
   has_many :colors, through: :color_scheme_colors
+
+  def colors_attributes=(colors_attributes)
+    colors_attributes.each do |i, color_attribute|
+      self.colors.find_or_initialize_by(color_attribute)
+    end
+  end
+
 end
