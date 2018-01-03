2864d61093a881cb59121dd7b269b3144fe92b9a
Travis Cole <iamtraviscole@gmail.com>
2018-01-03 00:46:57 -0500

    added json render to schemes controller, started on json get request for user's schemes

--- app/assets/javascripts/schemer.js
+++ app/assets/javascripts/schemer.js
@@ -3,13 +3,15 @@ $(function() {
 })
 
 function attachListeners(){
-  $('#yourschemeslink').on('click', function(event) {
+  $(document).on('click', '#yourschemeslink', function(event) {
     event.preventDefault()
-    console.log("clicked")
-    // loadColorSchemes()
+    loadColorSchemes()
   })
 }
 
 function loadColorSchemes() {
-
+  let userId = $('#current-user-data')[0].dataset.currentUserId
+  $.get(`/users/${userId}/color_schemes.json`, function(csData) {
+    console.log(csData)
+  })
 }
